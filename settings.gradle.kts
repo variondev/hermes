6737137a16fb5f8a2f1a1ec27c9e2fdba075a8f4
hypnot1q <afirmacion@proton.me>
2024-10-11 18:06:28 +0200

    Rename hermes-unit module to hermes-tests
    Refactor hermes-unit to hermes-tests for better clarity and organization. Updated all relevant file paths and module inclusions in the project settings to reflect the new naming convention.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 rootProject.name = "hermes"
 include(":hermes-common")
-include(":hermes-unit")
 include(":hermes-serdes")
 include(":hermes-serdes:hermes-serdes-jackson")
 include(":hermes-platforms")
 include(":hermes-platforms:hermes-platform-nats")
 include(":hermes-platforms:hermes-platform-redis")
+include(":hermes-tests")
