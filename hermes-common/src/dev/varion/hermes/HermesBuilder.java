6c8d8b313eb681f9feb8bcf10596774a1d3b5e4b
hypnot1q <afirmacion@proton.me>
2024-10-12 08:06:12 +0200

    Separate HermesBuilder into its own file
    Moved HermesBuilder from Hermes.java to a new HermesBuilder.java file. This improves the code organization by separating responsibilities into distinct files, making maintenance and readability easier.

---
+++ hermes-common/src/dev/varion/hermes/HermesBuilder.java
@@ -0,0 +1,95 @@
+package dev.varion.hermes;
+
+import static java.time.Duration.ofSeconds;
+
+import dev.shiza.dew.event.EventBus;
+import dev.varion.hermes.kv.KeyValueStorage;
+import dev.varion.hermes.locks.DistributedLocks;
+import dev.varion.hermes.message.MessageBroker;
+import dev.varion.hermes.packet.callback.PacketCallbackFacade;
+import dev.varion.hermes.packet.callback.requester.PacketCallbackRequester;
+import dev.varion.hermes.packet.pubsub.PacketPublisher;
+import dev.varion.hermes.packet.pubsub.PacketSubscriber;
+import dev.varion.hermes.packet.serdes.PacketSerdes;
+import java.time.Duration;
+
+public final class HermesBuilder {
+
+  private MessageBroker messageBroker;
+  private KeyValueStorage keyValueStorage;
+  private DistributedLocks distributedLocks;
+  private PacketSerdes packetSerdes;
+  private EventBus eventBus = EventBus.create().publisher(Runnable::run);
+  private Duration requestCleanupInterval = ofSeconds(10L);
+  private boolean shouldInitializeDistributedLocks;
+
+  HermesBuilder() {}
+
+  public HermesBuilder withMessageBroker(final MessageBroker messageBroker) {
+    this.messageBroker = messageBroker;
+    return this;
+  }
+
+  public HermesBuilder withKeyValueStorage(final KeyValueStorage keyValueStorage) {
+    this.keyValueStorage = keyValueStorage;
+    return this;
+  }
+
+  public HermesBuilder withDistributedLocks(final DistributedLocks distributedLocks) {
+    this.distributedLocks = distributedLocks;
+    return this;
+  }
+
+  public HermesBuilder withDistributedLocks(final boolean shouldInitializeDistributedLocks) {
+    this.shouldInitializeDistributedLocks = shouldInitializeDistributedLocks;
+    return this;
+  }
+
+  public HermesBuilder withPacketSerdes(final PacketSerdes packetSerdes) {
+    this.packetSerdes = packetSerdes;
+    return this;
+  }
+
+  public HermesBuilder withEventBus(final EventBus eventBus) {
+    this.eventBus = eventBus;
+    return this;
+  }
+
+  public HermesBuilder withRequestCleanupInterval(final Duration requestCleanupInterval) {
+    this.requestCleanupInterval = requestCleanupInterval;
+    return this;
+  }
+
+  public Hermes build() throws HermesException {
+    if (messageBroker == null) {
+      throw new HermesException("Message broker is missing while building Hermes.");
+    }
+    if (packetSerdes == null) {
+      throw new HermesException("Packet serdes is missing while building Hermes.");
+    }
+    if (keyValueStorage == null && (shouldInitializeDistributedLocks || distributedLocks != null)) {
+      throw new HermesException(
+          "Key value storage is required when distributed locks are provided.");
+    }
+
+    if (shouldInitializeDistributedLocks && distributedLocks == null) {
+      distributedLocks = DistributedLocks.create(keyValueStorage);
+    }
+
+    final PacketCallbackFacade packetCallbackFacade = PacketCallbackFacade.create();
+    final PacketPublisher packetPublisher = PacketPublisher.create(messageBroker, packetSerdes);
+    final PacketCallbackRequester packetCallbackRequester =
+        PacketCallbackRequester.create(
+            messageBroker, packetSerdes, packetCallbackFacade, requestCleanupInterval);
+    final PacketSubscriber packetSubscriber =
+        PacketSubscriber.create(
+            eventBus, messageBroker, packetPublisher, packetCallbackFacade, packetSerdes);
+    return new HermesImpl(
+        messageBroker,
+        keyValueStorage,
+        distributedLocks,
+        packetPublisher,
+        packetCallbackRequester,
+        packetSubscriber);
+  }
+}
