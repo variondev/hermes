35a03b46d9de744c1713d6fbf481ffb3bb19eaac
hypnot1q <afirmacion@proton.me>
2024-11-14 19:17:30 +0100

    Update test dependencies in build.gradle.kts
    Added new test dependencies for hermes-common and hermes-codec-jackson projects. Also updated dependencies for Jackson Databind and MessagePack libraries to ensure proper format support during testing. These changes enhance the testing capabilities of the project.

--- hermes-common/build.gradle.kts
+++ hermes-common/build.gradle.kts
@@ -8,10 +8,14 @@ plugins {
 dependencies {
     implementation("dev.shiza:dew:1.2.2")
     implementation("com.spotify:completable-futures:0.3.6")
+    testImplementation(project(":hermes-common"))
+    testImplementation(project(":hermes-codec-jackson"))
     testImplementation(project(":hermes-codec-msgpack-jackson"))
     testImplementation(project(":hermes-bridge-redis"))
     testImplementation(project(":hermes-bridge-nats"))
     testImplementation("io.lettuce:lettuce-core:6.4.0.RELEASE")
+    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.18.1")
+    testImplementation("org.msgpack:jackson-dataformat-msgpack:0.9.8")
     testImplementation("io.nats:jnats:2.20.2")
 }
 
